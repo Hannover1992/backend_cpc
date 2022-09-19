c47269de3d7bd673bc9134f299c542ec4d604c0a
PatrykKrzyzanski <Hannover1992@googlemail.com>
2022-09-19 16:37:28 +0200

    test if get work

--- tests/database.test.ts
+++ tests/database.test.ts
@@ -1,20 +1,14 @@
 import {PrismaClient} from "@prisma/client";
 import {describe, expect, test, beforeAll } from '@jest/globals';
 import {Projects} from "../source/projects";
+import {Project} from "../source/project";
 
 
-describe('CRUD', () => {
+describe('CRUD Project Direct', () => {
     let prisma: PrismaClient;
     let projects;
 
-    beforeAll(async () => {
-        prisma = new PrismaClient();
-        await prisma.project.deleteMany().catch(
-            (error) => {
-                expect(error).toBeNull();
-            }
-        );
-    });
+
 
     it("the project should be empty at the begin", async () => {
         await prisma.project.findMany().then(
@@ -55,6 +49,38 @@ describe('CRUD', () => {
             }
         );
     });
+
+    it("after inserting new project with Name TEST3, the project should be in the database", async () => {
+        await prisma.project.create({
+            data: {
+                Id: 3,
+                Location: "test3"
+            }
+        })
+        await prisma.project.findMany()
+            .then(
+                (projects) => {
+                    //project will contain the project with the name TEST3
+                    let project: Project;
+                    // @ts-ignore
+                    project = projects.find((project) => project.Location === "test3");
+                    expect(project).not.toBeNull();
+                    // @ts-ignore
+                    expect(project.Location).toBe("test3");
+                }
+            )
+        ;
+    });
+
+    afterAll(async () => {
+        prisma = new PrismaClient();
+        await prisma.project.deleteMany().catch(
+            (error) => {
+                expect(error).toBeNull();
+            }
+        );
+    });
+
 });
 
 describe('test the projects class', () => {
