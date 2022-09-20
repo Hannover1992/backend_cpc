aaa1e915995efc2ef015bd52618e69918e921975
PatrykKrzyzanski <Hannover1992@googlemail.com>
2022-09-20 11:38:24 +0200

    START create every ... as promise

--- tests/project.test.ts
+++ tests/project.test.ts
@@ -9,24 +9,8 @@ describe('test the projects class', () => {
         await project.Ready;
     });
 
-    it("test the project name", async () => {
-        expect(project.name).toBe("test");
-    });
-
-    it("test the project id", async () => {
-        expect(project.id).toBe(1);
-    });
-
-    it("when get the id direct from db, expect the same id", async () => {
-        expect(await project.get_id_direct_from_db()).toBe(1);
-    });
-
-    it("when get the name direct from db, expect the same name", async () => {
-        expect(await project.get_name_direct_from_db()).toBe("test");
-    });
-
-    it("test if after adding another project with the same id throw error Primary Key", async () => {
-
+    it('the id of the project should be instance of Promise<number>', () => {
+        expect(project.id).toBeInstanceOf(Promise);
     });
 
 
