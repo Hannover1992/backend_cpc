9bef77189a6fb78a17ef9e68ef104b82145bb32b
PatrykKrzyzanski <Hannover1992@googlemail.com>
2023-04-11 08:17:51 +0200

    everthing works fine, next: add error message when deliting project , so that, can' delete becouse something is booked of

--- source/Classes/Project.ts
+++ source/Classes/Project.ts
@@ -1,5 +1,5 @@
 import {ServerSetup} from "./ServerSetup";
-import {get_id_from_request} from "../Function/string_manipulation";
+import {get_id_from_request} from "./Function/string_manipulation";
 
 
 export class Project extends ServerSetup {
@@ -8,47 +8,12 @@ export class Project extends ServerSetup {
         super();
     }
 
-
-    //toDo: use this clean code
-
-    // private async createProject(data: any) {
-    //     try {
-    //         await this.prisma.tblprojekte.create({ data });
-    //         return true;
-    //     } catch (error) {
-    //         console.error("Error creating project:", error);
-    //         return false;
-    //     }
-    // }
-    //
-    // private sendSuccessResponse(res: any, message: string) {
-    //     res.status(200).send({ message });
-    // }
-    //
-    // private sendErrorResponse(res: any, message: string, statusCode: number = 500) {
-    //     res.status(statusCode).send({ message });
-    // }
-    //
-    // private create() {
-    //     this.app.post('/project/:id', async (req: any, res: any) => {
-    //         this.allow_communikation_from_all_ip_adress(res);
-    //         const success = await this.createProject(req.body);
-    //
-    //         if (success) {
-    //             this.sendSuccessResponse(res, "Project created");
-    //         } else {
-    //             this.sendErrorResponse(res, "An error occurred while creating the project");
-    //         }
-    //     });
-    // }
-
-
     create() {
         this.app.post('/project/:id', async (req: any, res: any) => {
             this.allow_communikation_from_all_ip_adress(res);
             this.prisma.tblprojekte.create({
                 data: req.body
-            }) .then((project: any) => {
+            }) .then(() => {
                 res.status(200).send({"message" : "Project created"});
             } ).catch((error: any) => {
                 res.status(500).send({"message": error.message});
@@ -71,7 +36,6 @@ export class Project extends ServerSetup {
             } );
         });
     }
-
 
     update() {
         this.app.put('/project/:id', (req: any, res: any) => {
@@ -96,6 +60,7 @@ export class Project extends ServerSetup {
         this.app.delete('/project/:id', (req: any, res: any) => {
             this.allow_communikation_from_all_ip_adress(res);
             let id = get_id_from_request(req);
+            console.log(id);
             this.prisma.tblprojekte.delete({
                 where: {
                     ID: id
@@ -107,7 +72,6 @@ export class Project extends ServerSetup {
             });
         });
     }
-
 }
 
 
