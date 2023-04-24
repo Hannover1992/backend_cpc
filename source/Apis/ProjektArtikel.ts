83c43ff56cc698ef03a7e715fd28614f20aa441a
PatrykKrzyzanski <Hannover1992@googlemail.com>
2023-04-24 15:30:16 +0200

    looks good

---
+++ source/Apis/ProjektArtikel.ts
@@ -0,0 +1,61 @@
+import {ServerSetup} from "../ServerSetup";
+
+export class ProjektArtikel extends ServerSetup {
+
+    constructor() {
+        super();
+    }
+
+    create(...args: any[]): any {
+    }
+
+    deletee(...args: any[]): any {
+    }
+
+    read(...args: any[]): any {
+        this.app.get('/project/:id/artikel', async (req: any, res: any) => {
+            this.allow_communikation_from_all_ip_adress(res);
+            let id = get_id_from_request(req);
+            this.getFilteredProjektArtikel(id, req.query.unterkategoriename)
+                .then((artikel: any) => {
+                res.status(200).send(artikel);
+            }).catch((error: any) => {
+                res.status(500).send({"message": error.message});
+            });
+        });
+    }
+
+
+    async getFilteredProjektArtikel(projekt_id: any, unterkategoriename: any) {
+        const result = await this.prisma.projekt_artikel.findMany({
+            where: {
+                projekt_artiekl_id: projekt_id,
+                artikel: {
+                    unterkategorie: {
+                        unterkategoriename: {
+                            contains: unterkategoriename,
+                        },
+                    },
+                },
+            },
+            include: {
+                artikel: {
+                    select: {
+                        artikel_id: true,
+                        artikelname: true,
+                        unterkategorie: true,
+                    },
+                },
+            },
+        });
+
+        return result;
+    }
+
+
+
+    update(...args: any[]): any {
+    }
+
+
+}
--- source/index.ts
+++ source/index.ts
@@ -2,11 +2,13 @@ import {Project} from "./Apis/Project";
 import {ProjectTable} from "./Apis/ProjectTable";
 import {KategorienTable} from "./Apis/KategorienTable";
 import {Asset} from "./Apis/Asset";
+import {ProjektArtikel} from "./Apis/ProjektArtikel";
 
 
 async function start() {
     new Project();
     new ProjectTable();
+    new ProjektArtikel();
     new KategorienTable();
     new Asset();
 }
